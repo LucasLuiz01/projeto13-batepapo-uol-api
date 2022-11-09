4edcb57fe831b68fe07b7dbf0b836056531ed053
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-11-09 15:06:01 -0300

    bibliotecas instaladas

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors"
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(5000);
+
