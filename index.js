576c474a1330415451f9b0884b5fc300fd2e2226
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-11-09 23:10:43 -0300

    configuracoes iniciais

--- index.js
+++ index.js
@@ -2,12 +2,36 @@ import express from "express";
 import cors from "cors"
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
-
+//Configurando dotenv
+dotenv.config();
+//Configurando APP
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Configurando Mongo
+const mongoClient = new MongoClient(process.env.MONGO_URL)
 
+
+
+app.post("/participantes", (req, res) => {
+
+})
+
+app.post("/messages", (req, res) => {
+
+})
+app.post("/status", (req, res) => {
+
+})
+
+app.get("/participantes", (req, res) => {
+
+})
+
+app.get("/messages", (req, res) => {
+
+})
 
 app.listen(5000);
 
