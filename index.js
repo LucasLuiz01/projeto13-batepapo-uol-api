a29325739d607f59219fed29d35341453540b0f4
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-11-10 00:36:47 -0300

    funcoes iniciais funcionando

--- index.js
+++ index.js
@@ -11,26 +11,72 @@ app.use(cors());
 
 //Configurando Mongo
 const mongoClient = new MongoClient(process.env.MONGO_URL)
-
-
-
-app.post("/participantes", (req, res) => {
-
+//Criando db
+let db;
+//Conectando mongo
+mongoClient.connect().then(() => {
+    db = mongoClient.db("batepapo");
 })
 
-app.post("/messages", (req, res) => {
+
+//Rotas post
+app.post("/participantes", async (req, res) => {
+    const {name} = req.body;
+    if(!name){
+        res.status(422).send("Insira um nome válido")
+        return;
+    } 
+    try{
+       await db.collection("participante").insertOne({name, lastStatus: Date.now()})
+       res.status(201).send("Participante Criado");
+    } catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+})
+
+app.post("/messages", async (req, res) => {
+    const {to, text, type} = req.body;
+    const {user} = req.headers;
+    try{
+        await db.collection("messages").insertOne({from: user, to, text, type,})
+        res.status(201).send("Mensagem enviada");
+    } catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+
 
 })
-app.post("/status", (req, res) => {
-
+app.post("/status", async (req, res) => {
+   const {user} = req.headers;
+   try{
+   
+} catch(err){
+    console.log(err);
+    res.sendStatus(500);
+}
 })
 
-app.get("/participantes", (req, res) => {
-
+//Rotas get
+app.get("/participantes", async (req, res) => {
+try{
+    let listaParticipantes = await db.collection("participante").find().toArray()
+    res.status(201).send(listaParticipantes);
+} catch(err) {
+    console.log(err);
+    res.sendStatus(500);
+}
 })
 
-app.get("/messages", (req, res) => {
-
+app.get("/messages", async (req, res) => {
+    try{
+    const listaMessages = await db.collection("messages").find().toArray();
+    res.status(201).send(listaMessages)
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 })
 
 app.listen(5000);
